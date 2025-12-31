aec6b8608302a66f5c68966e4ff9cbbb0c0fed13
DevarasettiKarthik <karthikdevarasetti02@gmail.com>
2025-12-31 13:36:44 +0530

    Create Bsearch.c

---
+++ Bsearch.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int binarySearch(int arr[], int n, int key) {
+    int low = 0, high = n - 1;
+
+    while (low <= high) {
+        int mid = (low + high) / 2;
+
+        if (arr[mid] == key)
+            return mid;
+        else if (arr[mid] < key)
+            low = mid + 1;
+        else
+            high = mid - 1;
+    }
+    return -1;
+}
+
+int main() {
+    int n, key;
+    scanf("%d", &n);
+
+    int arr[n];
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    scanf("%d", &key);
+
+    int result = binarySearch(arr, n, key);
+
+    if (result != -1)
+        printf("%d", result);
+    else
+        printf("-1");
+
+    return 0;
+}
